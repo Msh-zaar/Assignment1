f6783009ba878f586a041ce975f86993f0fcb0ff
Michael Zaar <60154804+Msh-zaar@users.noreply.github.com>
2022-01-14 21:03:55 +0100

    feat: added abstract character class and base char classes

---
+++ Assignment1/Classes/Mage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Mage : Character
+    {
+        public override void LevelUp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Assignment1
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
